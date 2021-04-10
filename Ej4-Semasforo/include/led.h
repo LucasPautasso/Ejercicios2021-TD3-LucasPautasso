49f6101e4c14f3d0574ff1f3cb8ae56d8bdc1ad3
LucasPautasso <lucaspaut@gmail.com>
2021-04-10 20:40:06 -0300

    actualiza ejercicio 4

--- Ej4-Semasforo/include/led.h
+++ Ej4-Semasforo/include/led.h
@@ -1,17 +1,17 @@
 #ifndef LED_H //chequea que otro archivo no llame a led.h
 #define LED_H
 
-#define VERDE GPIO_NUM_25
-#define AMARILLO 1
-#define ROJO 2
+#include "driver/gpio.h"
 
-int LedR = 32;
-int LedA = 26;
-int LedV = 25;
+//int LedR = 32;
+//int LedA = 26;
+//int LedV = 25;
 
-void configurarLed(int LedR, int LedA, int LedV);
+//void configurarLed(int LedR, int LedA, int LedV);
+void configurarLed(gpio_int_type_t ledR, gpio_int_type_t ledA, gpio_int_type_t ledV); //especifica en que pines conectar cada led: ROJO, AMARILLO, VERDE
 
-void prenderLed();
-void apagarLed();
+void prenderLed(char led);
+
+void apagarLed(char led);
 
 #endif
--- Ej4-Semasforo/src/semasforo.c
+++ Ej4-Semasforo/src/semasforo.c
@@ -1,24 +1,34 @@
 #include "../include/led.h"
 #include "../include/semasforo.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define LED_VERDE GPIO_NUM_25
+#define LED_AMARILLO GPIO_NUM_26
+#define LED_ROJO GPIO_NUM_27
+
+#define T_VERDE		300
+#define T_AMARILLO	100
+#define T_ROJO		500
 
 void iniciarSemasforo()
 {
-
+    configurarLed(LED_ROJO, LED_AMARILLO, LED_VERDE);
 }
 
 void actualizarSemasforo()
 {
-    prenderLed (ROJO);
-    vTaskDelay(50);
-    prenderLed (AMARILLO);
-    vTaskDelay(50);
-    apagarLed (ROJO);
-    apagarLed (AMARILLO);
-    prenderLed (VERDE);
-    vTaskDelay(50);
-    apagarLed (VERDE);
-    prenderLed (AMARILLO);
-    vTaskDelay(50);
-    apagarLed (AMARILLO);
+    prenderLed ('R');
+    vTaskDelay(T_ROJO);
+    prenderLed ('A');
+    vTaskDelay(T_AMARILLO);
+    apagarLed ('R');
+    apagarLed ('A');
+    prenderLed ('V');
+    vTaskDelay(T_VERDE);
+    apagarLed ('V');
+    prenderLed ('A');
+    vTaskDelay(T_AMARILLO);
+    apagarLed ('A');
 
 }
